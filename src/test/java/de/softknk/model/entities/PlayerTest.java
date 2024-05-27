a3b3213f6ae6ffaaa82fcb6124826c0ae237af3a
mikzb <mikolajzabski@uma.es>
2024-05-27 20:53:26 +0200

    Added required dependencies, PlayerTest 5/6PASS

--- src/test/java/de/softknk/model/entities/PlayerTest.java
+++ src/test/java/de/softknk/model/entities/PlayerTest.java
@@ -1,13 +1,20 @@
 package de.softknk.model.entities;
 import static org.junit.jupiter.api.Assertions.*;
+
+import javafx.application.Platform;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
-    private Player player;
-
+    private static Player player;
+    static //static block only runs once when the class is loaded
+    {
+        Platform.startup(() -> {
+        });
+    }
     @BeforeEach
     public void setUp() {
         player = new Player("TestPlayer", 0);
@@ -34,6 +41,7 @@ public class PlayerTest {
 
     @Test
     @DisplayName("When you increase the score of a player with a negative score, the score should not change.")
+    //todo: but it do
     public void increaseScore_WithNegativeScore_DoesNotChangeScore() {
         player.increaseScore(-5);
         assertEquals(0, player.getScore());
